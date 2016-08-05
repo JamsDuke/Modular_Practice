d7600afeada8d195bbccdbfa141b7c3e8f58bd7c
Jamie Duke <jamsduke@gmail.com>
2016-08-05 11:31:30 -0500

    initial files and code

---
+++ script.js
@@ -0,0 +1,13 @@
+var myModule = {
+  name: 'Will',
+  age: 34,
+  sayName: function() {
+    alert(this.name);
+  },
+  setName: function(newName) {
+    this.name = newName;
+  }
+};
+
+myModule.setName('Willis');
+myModule.sayName();
